c8019b28eabcc5ac1545c2c2c646d59d19d423ec
garydid <gary@Garys-MacBook-Air.local>
2015-06-22 18:49:38 -0700

    nav displaying on click

---
+++ week 4/web app/js/nav.js
@@ -0,0 +1,25 @@
+
+
+
+var hamburger = document.getElementById('hamburger');
+
+var navContainer = document.getElementById('navContainer');
+
+var container = document.getElementById('container');
+
+hamburger.addEventListener("click", function(){
+
+  // navContainer.style.display = "block";
+
+  // container.style.width = "50%";
+  if( navContainer.style.display == "")
+
+    { navContainer.style.display = "block"; }
+
+    else if( navContainer.style.display == "block")
+
+      { navContainer.style.display = ""; }
+
+
+
+});
--- week 4/web app/js/slider.js
+++ week 4/web app/js/slider.js
@@ -1,66 +1,98 @@
-// Define prototypical Slider function
-
+//Define prototypical Slider function
 Element.prototype.Slider = function(){
-  var slider = this;
-  var wrapper = slider.children[0];
-  var slides = wrapper.children;
-  var position = 0;
-  var width = window.innerWidth;
-  var leftButton = document.createElement('div');
-  var rightButton = document.createElement('div');
 
-  this.createButtons = function() {
+  // Define global variables
 
-    leftButton.classList.add('left');
-    rightButton.classList.add('right');
+  /* slider = the element (<div>) that calls Slider() function (or 'this').
+     this is the instance of the element
+     wrapper equals the <ul> element inside <div class="slider">
+     slides equal the array of <li> elements inside <ul>
+  */
+      // search component for elements
+  var slider = this,
+      wrapper = slider.children[0],
+      slides = wrapper.children,
+      // initialize position, width
+      position = 0,
+      width = window.innerWidth,
+      // create two divs for buttons
+      leftBtn = document.createElement('div'),
+      rightBtn = document.createElement('div');
 
-    slider.appendChild(leftButton);
-    slider.appendChild(rightButton);
+  // Create buttons for sliding the Slider left and right
+
+  this.createButtons = function(){
+
+    // add classes to buttons for styling
+
+    leftBtn.classList.add('left');
+    rightBtn.classList.add('right');
+
+    // add EventListeners to buttons, activating the CSS transition
+
+    leftBtn.addEventListener('mousedown',function(){
+      if(position > (width * (slides.length - 1)) * -1){
+        position = position - width;
+        wrapper.style.marginLeft = position + "px";
+      }
+    });
+
+    rightBtn.addEventListener('mousedown',function(){
+      if(position < 0){
+        position = position + width;
+        wrapper.style.marginLeft = position + "px";
+      }
+    });
+
+    // append the buttons to the Slider
+
+    slider.appendChild(leftBtn);
+    slider.appendChild(rightBtn);
 
   };
 
-  this.resize = function() {
+  // Resize the wrapper, slides by looping through the slides and finding the shortest.
 
+  this.resize = function(){
+
+    // change the width to 100% of the document Window.
     width = window.innerWidth;
 
-    wrapper.style.width = slides.length * width + 'px';
-    wrapper.style.height = '100%';
+    // change the width of the <ul> to be the width of all slides combined.
+    wrapper.style.width = width * slides.length + "px";
+    // reset the position of the wrapper
+    wrapper.style.marginLeft = "0px";
 
-  };
+    // loop through slides and find the shortest, change the height of the wrapper to the shortest
+    for(var index=0; index < slides.length; index++){
 
+      slides[index].children[0].style.width = width + "px";
+      slides[index].style.width = width + "px";
+      slides[index].style.height = slides[index].children[0].clientHeight + "px";
 
-  var init = function() {
+      if(slides[index].children[0].clientHeight <= slides[0].children[0].clientHeight || index===0){
+        wrapper.style.height = slider.style.height = slides[index].children[0].clientHeight + "px";
+        leftBtn.style.marginTop = rightBtn.style.marginTop = -1*(slides[index].children[0].clientHeight / 2) - 20 + 'px';
+      }
 
-
-    for(var i=0; i<slides.length; i++) {
-      slides[i].style.width = width + 'px';
 
     }
 
-    leftButton.addEventListener('mousedown',function(){
+  }
 
-      if(position > (width * (slides.length - 1)) * -1){
-        position = position - width;
-        wrapper.style.marginLeft = position + 'px';
-      }
+  this.init = function(){
 
-    });
+    slider.resize();
+    slider.createButtons();
 
-
-    rightButton.addEventListener('mousedown',function(){
-
-      if(position < 0) {
-      position = position + width;
-      wrapper.style.marginLeft = width +'px';
-      }
-
-
-    });
-
+    // on resize of the browser window, call the resize function.
+    window.addEventListener('resize',slider.resize);
 
   };
 
-  init();
+
+  this.init(); // do tasks on initialization.
+
 
 };
 /* end Slider */
